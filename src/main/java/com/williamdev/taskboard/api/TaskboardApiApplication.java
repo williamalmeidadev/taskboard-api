5e32d901ec5a6ad8b6f216fdf1e6770e5ee96680
William Almeida <williamalmeida300000@gmail.com>
2026-01-02 22:20:54 -0300

    structure

---
+++ src/main/java/com/williamdev/taskboard/api/TaskboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.williamdev.taskboard.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskboardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskboardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/williamdev/taskboard/api/TaskboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamdev.taskboard.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
